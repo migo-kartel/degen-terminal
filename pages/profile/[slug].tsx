ade46f49378cad68ec71bdf149b66407df4ef8a7
Juan <juanito.lansangan@gmail.com>
2024-04-19 23:46:37 +0800

    update

--- pages/profile/[slug].tsx
+++ pages/profile/[slug].tsx
@@ -240,14 +240,14 @@ export default function Profile({ profile }) {
                 onClick={() => setFeedType(FeedTypeEnum[key])}
               >
                 {FeedTypeEnum[key]}
-                {feedType !== FeedTypeEnum[key] && (
+                {/* {feedType !== FeedTypeEnum[key] && (
                   <div className="ml-4 text-foreground/80 hidden md:block">
                     Switch with &nbsp;
                     <kbd className="px-1.5 py-1 text-xs border rounded-lg bg-foreground/80 text-background/80">
                       Tab
                     </kbd>
                   </div>
-                )}
+                )} */}
               </TabsTrigger>
             );
           })}
