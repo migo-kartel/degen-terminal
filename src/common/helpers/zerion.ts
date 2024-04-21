b905622b3e3cf57f1a15742b35d4ccfe02a9163a
Juan <juanito.lansangan@gmail.com>
2024-04-21 17:09:50 +0800

    update

--- src/common/helpers/zerion.ts
+++ src/common/helpers/zerion.ts
@@ -2,16 +2,11 @@ import axios from "axios";
 
 export const getPortfolio = async (ethAddress: string) => {
     const url = process.env.NEXT_PUBLIC_ZERION_URL;
-    const username = process.env.NEXT_PUBLIC_ZERION_API_KEY;
-    const password = '';
-    const basicAuth = 'Basic ' + btoa(username + ':' + password);
-
     const options = {
         method: 'GET',
         url: `${url}/crypto-wallet/${ethAddress}`,
         headers: {
           accept: 'application/json',
-          authorization: basicAuth
         }
     };
     
