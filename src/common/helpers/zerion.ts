825baa168ec763328996b659add3c3446a9edc0e
Juan <juanito.lansangan@gmail.com>
2024-04-21 16:59:13 +0800

    fix cors

--- src/common/helpers/zerion.ts
+++ src/common/helpers/zerion.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 export const getPortfolio = async (ethAddress: string) => {
+    const url = process.env.NEXT_PUBLIC_ZERION_URL;
     const username = process.env.NEXT_PUBLIC_ZERION_API_KEY;
     const password = '';
     const basicAuth = 'Basic ' + btoa(username + ':' + password);
 
     const options = {
         method: 'GET',
-        url: `https://api.zerion.io/v1/wallets/${ethAddress}/portfolio`,
+        url: `${url}/crypto-wallet/${ethAddress}`,
         headers: {
           accept: 'application/json',
           authorization: basicAuth
